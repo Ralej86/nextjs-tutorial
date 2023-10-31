f3de286b4a96c104a15de5cd5783efcea6288e1e
Ryan <alejandr257@gmail.com>
2023-10-31 16:29:36 -0700

    setup basic shell

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
-import Image from "next/image";
 import Head from "next/head";
 import { Inter } from "next/font/google";
+
+import { PostCard, Categories, PostWidget } from "@/components";
+
+const posts = [
+    { title: "React Testing", excerpt: "Learn React Testing" },
+    { title: "React With Tailwind", excerpt: "Learn React with Tailwind" },
+];
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,6 +16,19 @@ export default function Home() {
             <Head>
                 <title>Create Next App</title>
             </Head>
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+                <div className="col-span-1 lg:col-span-8">
+                    {posts.map((post, idx) => (
+                        <PostCard key={idx} post={post} />
+                    ))}
+                </div>
+                <div className="col-span-1 lg:col-span-4">
+                    <div className="lg:sticky relative top-8">
+                        <PostWidget />
+                        <Categories />
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
